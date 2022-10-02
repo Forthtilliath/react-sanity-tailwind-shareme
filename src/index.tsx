4601a99bc7f7b9d13b9fe13c4bc6d707ca3fd438
Forth <33458877+Forthtilliath@users.noreply.github.com>
2022-10-02 21:22:29 +0200

    index to tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import React, { FunctionComponent, PropsWithChildren } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -8,20 +9,28 @@ import { UserProvider } from './utils/contexts/UserContext';
 import App from './App';
 import './index.css';
 
-function AppWithProviders({ children }) {
+const AppWithProviders: FunctionComponent<PropsWithChildren> = ({
+  children,
+}) => {
+  if (!process.env.REACT_APP_PUBLIC_GOOGLE_API_TOKEN)
+    return (
+      <p>
+        <strong>REACT_APP_PUBLIC_GOOGLE_API_TOKEN</strong> Not Found!
+      </p>
+    );
   return (
     <SearchProvider>
       <UserProvider>
         <GoogleOAuthProvider
-          clientId={process.env.REACT_APP_PUBLIC_GOOGLE_API_TOKEN}>
+          clientId={process.env.REACT_APP_PUBLIC_GOOGLE_API_TOKEN!}>
           {children}
         </GoogleOAuthProvider>
       </UserProvider>
     </SearchProvider>
   );
-}
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <BrowserRouter>
     <AppWithProviders>
